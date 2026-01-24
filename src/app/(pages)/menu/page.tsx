880c0c5e849e7a0662a673bf3c638f0c1f65ed09
exuseric <ericbro29@gmail.com>
2026-01-24 13:34:38 +0300

    feat(menu): implement PageHero component and update menu page layout

--- src/app/(pages)/menu/page.tsx
+++ src/app/(pages)/menu/page.tsx
@@ -1,7 +1,9 @@
+import PageHero from "@/components/shared/page-hero/PageHero";
+
 export default function MenuPage() {
   return (
-    <div>
-      <h1>Menu Page</h1>
-    </div>
+    <section className="menu">
+      <PageHero heading="Happy Hour" description="Everyday from 4pm to 7pm" src="https://r6niw2f0z8.ufs.sh/f/tcbNvrChZBJK6bybvAMpIanNzZiHsRMQtC4AockwVLxfTGu1" alt="cocktail" />
+    </section>
   );
 }
