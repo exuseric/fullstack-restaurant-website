ec7caaf3d6c82d49a2f483d208909ecd76f81b98
exuseric <ericbro29@gmail.com>
2026-01-25 16:34:45 +0300

    refactor(url-params): format imports and improve readability of parsers object definition

--- src/lib/url-params.ts
+++ src/lib/url-params.ts
@@ -2,12 +2,12 @@
 import { createSearchParamsCache, parseAsArrayOf, parseAsInteger, parseAsString } from "nuqs/server";
 
 export const searchParamsParsers = {
-    category: parseAsArrayOf(parseAsInteger).withDefault([]),
-    query: parseAsString.withDefault(''),
-    minPrice: parseAsInteger.withDefault(0),
-    maxPrice: parseAsInteger.withDefault(2000),
-    page: parseAsInteger.withDefault(1),
-} as const
+  category: parseAsArrayOf(parseAsInteger).withDefault([]),
+  query: parseAsString.withDefault(""),
+  minPrice: parseAsInteger.withDefault(0),
+  maxPrice: parseAsInteger.withDefault(2000),
+  page: parseAsInteger.withDefault(1),
+} as const;
 
 export type SearchParamsType = typeof searchParamsParsers;
 
