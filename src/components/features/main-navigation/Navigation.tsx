910b942e2d45d665168aefaa8c6e3e7d67143c25
exuseric <ericbro29@gmail.com>
2026-02-01 18:49:56 +0300

    refactor(navigation): colocate link retrieval logic
    colocate `getNavigationLinks` logic by moving it from the general `src/use-cases/navigation` directory to
    `src/components/features/main-navigation/lib`.
    
    This change:
    - improves module cohesion by placing component-specific logic closer to its consumer
    - removes the now-empty `src/use-cases/navigation` module and its index files
    - deletes `src/use-cases/test/usecases.test.ts` as its related logic was removed

--- src/components/features/main-navigation/Navigation.tsx
+++ src/components/features/main-navigation/Navigation.tsx
@@ -1,4 +1,4 @@
-import { getNavigationLinks } from "@/use-cases/navigation";
+import { getNavigationLinks } from "./lib/get-navigation-links";
 import NavigationContainer from "./NavigationContainer";
 
 export default async function MainNavigation() {
--- src/use-cases/index.ts
+++
@@ -1,1 +0,0 @@
-export * as navigationUseCases from "./navigation";
